7762ec5c9075d17f285e2053c695e5bd46b73fd0
krishna29082005 <krishnab2908@gmail.com>
2026-06-17 13:49:44 +0530

    Time: 18 ms (55.4%), Space: 26.5 MB (19.81%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,19 +3,18 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)return -1;
-        priority_queue<pair<int , pair<int , int>>, 
-        vector<pair<int , pair<int , int>>>,
-        greater<pair<int , pair<int , int>>>>pq;//declaring a min heap;
+        queue<pair<int , pair<int , int>>>pq;
         vector<vector<int>>vis(n , vector<int>(n , INT_MAX));
         pq.push({1 ,{ 0 , 0}});
         vis[0][0] = 1;
         int delr[8] = {-1 , 0 , 1 , 0 , -1 , -1 , 1 , 1};
         int delc[8] = {0 , 1 , 0 , -1 , -1 , 1 , 1 , -1};
         while(!pq.empty()){
-            auto temp = pq.top();
+            auto temp = pq.front();
             int dis = temp.first;
             int ni = temp.second.first;
             int nj = temp.second.second;
+            if(ni == n - 1 && nj == n-1) return dis;
             pq.pop();
              
             for(int i = 0 ; i < 8 ; i++){
@@ -28,9 +27,6 @@ public:
                 }
             }
         }
-        
-        int ans = vis[n - 1][n - 1];
-        if(ans == INT_MAX)return -1;
-        return ans;
+        return -1;
     }
 };
