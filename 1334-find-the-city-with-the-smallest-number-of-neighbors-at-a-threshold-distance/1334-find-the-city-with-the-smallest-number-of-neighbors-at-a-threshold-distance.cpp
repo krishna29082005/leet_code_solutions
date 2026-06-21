152e9161d8c401b8d0158677588c9449d8393ad6
krishna29082005 <krishnab2908@gmail.com>
2026-06-21 13:46:19 +0530

    Time: 19 ms (34.05%), Space: 20.7 MB (11.69%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,38 +1,51 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>mat(n , vector<int>(n , 1e9));
-        for(int i = 0; i < n; i++) {
-          mat[i][i] = 0;
-        }
-        for(auto it : edges){
-            int u = it[0];
-            int v = it[1];
-            int wt = it[2];
-            mat[u][v] = wt;
-            mat[v][u] = wt;
-        }
-        for(int via = 0 ; via < n ; via++){
-            for(int i = 0 ; i < n ; i++){
-                for(int j = 0 ; j < n ; j++){
-                    if(mat[i][via] != 1e9 && mat[via][j] != 1e9){
-                    mat[i][j] = min(mat[i][j] , mat[i][via] + mat[via][j]);
-                    }
-                }
-            }
+        vector<vector<pair<int , int>>>adj(n);
+        int e = edges.size();
+        for(int i = 0 ; i < e  ; i++)
+        {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+            adj[u].push_back({v , wt});
+            adj[v].push_back({u , wt});
         }
         int mini = INT_MAX;
         int ans = -1;
-        for(int i = 0 ; i < n ; i++){
-            int cnt = 0;
-            for(int j = 0 ; j < n ; j++){
-                if(mat[i][j] <= distanceThreshold){
-                  cnt++;
+        for(int i = 0; i < n; i++) {
+
+
+        vector<int>dis(n , INT_MAX);
+        priority_queue<pair<int , int>,
+                       vector<pair<int , int>>,
+                       greater<pair<int , int>>>pq;
+        pq.push({0 , i});
+        dis[i] = 0;
+
+        while(!pq.empty()){
+            int curdis = pq.top().first;
+            int temp = pq.top().second;
+            pq.pop();
+            if(curdis > dis[temp]) continue;
+            for(auto it : adj[temp]){
+                int t_temp = it.first;
+                int t_dis = it.second;
+                if(dis[temp] + t_dis < dis[t_temp]){
+                    dis[t_temp] = dis[temp] + t_dis;
+                    pq.push({dis[temp] + t_dis , t_temp});
                 }
             }
-            if(cnt <= mini){
-                mini = cnt;
-                ans = i;
+        }
+        int cnt = 0;
+        for(int j = 0 ; j < n ; j++){
+            if(dis[j] <= distanceThreshold){
+                  cnt++;
+                }
+        }
+        if(cnt <= mini){
+            mini = cnt;
+            ans = i;
             }
         }
         return ans;
