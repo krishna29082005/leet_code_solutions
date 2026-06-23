af8a2fd449114cbdac816ace00418009de3373e4
krishna29082005 <krishnab2908@gmail.com>
2026-06-23 20:53:19 +0530

    Time: 226 ms (22.61%), Space: 182.3 MB (47.89%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -57,9 +57,11 @@ class Solution {
 private:
     int timer = 1;
     void dfs(int node , int parent , vector<int> &vis , vector<vector<int>> & adj , int tim[] , int low[] , vector<vector<int>>& bridges){
+        if(vis[node] == 1) return;
         vis[node] = 1;
         tim[node] = low[node] = timer;
         timer++;
+    
         for(auto it : adj[node]){
             if(it == parent) continue;
             if(vis[it] == 0){
@@ -69,7 +71,6 @@ private:
                   bridges.push_back({it , node});
                }
             } 
-            
                 low[node] = min(low[node] , low[it]);
             
         }
