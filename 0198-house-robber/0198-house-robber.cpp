6b6f684590fa323f232ec8e9ec1a3650a5a97ab8
krishna29082005 <krishnab2908@gmail.com>
2026-06-25 14:53:06 +0530

    Time: 0 ms (100%), Space: 10.6 MB (62.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,22 @@
 class Solution {
 private:
-        int money(int n , vector<int>&dp , vector<int>& nums){
-            if(n == 0) return nums[0];
-            if(n < 0) return 0;
-            if(dp[n] != -1) return dp[n];
-            int pick = nums[n] + money(n - 2 , dp , nums);
-            int nonpick = 0 + money(n - 1 , dp , nums);
-            return dp[n] = max(pick , nonpick);
-        }
+        
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n , -1);
         if(n == 1) return nums[0];
-        int ans = money(n - 1 , dp , nums);
-        return ans;
+        dp[0] = nums[0];
+        dp[1] = max(nums[1] , nums[0]);
+        for(int i = 2 ; i < n ; i++){
+    
+            int pick = nums[i] + dp[i - 2];
+            int nonpick = 0 + dp[i - 1];
+
+            dp[i] = max(pick , nonpick);
+        }
         
+        return dp[n - 1];
     }
 };
 
