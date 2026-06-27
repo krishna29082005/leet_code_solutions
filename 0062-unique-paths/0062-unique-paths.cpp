b9e5e57326d752b98c52088a111a0fc012440574
krishna29082005 <krishnab2908@gmail.com>
2026-06-27 19:22:42 +0530

    Time: 2 ms (21.1%), Space: 8.4 MB (72.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,43 +1,33 @@
 class Solution {
 private: 
-    int f(int i , int j , vector<vector<int>>&dp , int m , int n){
-        if(i == 0 && j == 0) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        int up = 0;
-        int left = 0;
-        if(i - 1 >= 0 && i - 1 < m) // going up
-        {
-            up = f(i - 1 , j , dp , m , n);
-        }
-        
-        if(j - 1 >= 0 && j - 1 < n) // going left
-        {
-            left = f(i , j - 1 , dp , m , n);
-        }
-        return dp[i][j] = up + left;
-    }
     
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m , vector<int>(n , -1));
+        vector<int>dp(n , -1);
         if(m == 1 && n == 1) return 1;
-        dp[0][0] = 1;
+        dp[0] = 1;
         for(int i = 0 ; i < m ; i++){
+            vector<int> temp(n , -1);
             for(int j = 0 ; j < n ; j++){
-                if(i == 0 &&j == 0) continue;
+                if(i == 0 &&j == 0) 
+                {
+                    temp[j] = 1;
+                    continue;
+                }
                 int up = 0;
                 int left = 0;
                 if(i - 1 >= 0 && i - 1 < m) // going up
                 {
-                    up = dp[i - 1][j];
+                    up = dp[j];
                 }
                if(j - 1 >= 0 && j - 1 < n) // going left
                 {
-                    left = dp[i][j - 1];
+                    left = temp[j - 1];
                 } 
-                dp[i][j] = up + left;
+                temp[j] = up + left;
             }
+            dp = temp;
         }
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 };
