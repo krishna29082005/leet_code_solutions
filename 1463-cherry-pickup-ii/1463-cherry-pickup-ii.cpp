a841d4bb2c468d6f5f576db7bc25f10da64f91bd
krishna29082005 <krishnab2908@gmail.com>
2026-06-29 13:21:32 +0530

    Time: 40 ms (77.75%), Space: 21.2 MB (12.73%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,47 +1,4 @@
 class Solution {
-private:
-    int f(int row , int col1 , int col2 , vector<vector<int>>& grid,
-          vector<vector<vector<int>>>& dp ){
-
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if(col1 < 0 || col1 >= n || col2 < 0 || col2 >= n)
-        return -1e9;//boundary;
-        
-        if(dp[row][col1][col2] != -1) return dp[row][col1][col2];//dp;
-        
-        if(row == m - 1){//base case
-            if(col1 == col2) return dp[row][col1][col2] = grid[row][col1];
-            else
-            return dp[row][col1][col2] = grid[row][col1] + grid[row][col2];
-        }
-        
-        
-        int reward = 0;
-        if(col1 == col2) //collecting current reward
-           reward = grid[row][col1];
-        else {
-            reward = grid[row][col1] + grid[row][col2];
-        }
-
-        int dc[3] = {-1 , 0 , 1};//collecting max of all the 9 combinations
-        int dc2[3] = {-1 , 0 , 1};
-        
-        int maxi = INT_MIN;
-        for(int i = 0 ; i < 3 ; i++){
-            int nc1 = col1 + dc[i];
-            for(int j = 0 ; j < 3 ; j++){
-                int nc2 = col2 + dc2[j];
-                if(nc1 >= 0 && nc1 < n && nc2 >= 0 && nc2 < n){
-                    int nextreward = f(row + 1 , nc1 , nc2 , grid , dp);
-                    int totalreward = reward + nextreward;
-                    maxi = max(maxi , totalreward);
-            }       
-        }
-        }
-        return dp[row][col1][col2] = maxi;
-}
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size();
@@ -51,10 +8,46 @@ public:
                              vector<vector<int>>( 
                              n,  
                              vector<int>(n , -1)));
-
+    
         
-        int ans = f(0 , 0 , n - 1 , grid , dp);
-        return ans;
+        for(int col1 = 0; col1 < n; col1++) {
+            for(int col2 = 0; col2 < n; col2++) {
+
+            if(col1 == col2)
+               dp[m-1][col1][col2] = grid[m-1][col1];
+            else
+               dp[m-1][col1][col2] = grid[m-1][col1] + grid[m-1][col2];
+        }
+    }
+        
+    for(int row = m - 2 ; row >=0 ; row--){
+       for(int col1 = 0; col1 < n; col1++) {
+          for(int col2 = 0; col2 < n; col2++) {
+                int maxi = INT_MIN;
+                int reward = 0;
+                if(col1 == col2)
+                   reward = grid[row][col1];
+                else
+                   reward = grid[row][col1] + grid[row][col2];
+
+        for(int d1 = -1 ; d1 < 2 ; d1++){
+            for(int d2 = -1 ; d2 < 2 ; d2++){
+                int newcol1 = col1 + d1;
+                int newcol2 = col2 + d2;
+                int nextreward = 0;
+                if(newcol1 >= 0 && newcol1 < n && newcol2 >= 0 && newcol2< n)
+                    {
+                        nextreward = dp[row + 1][newcol1][newcol2];
+                        int totalreward = reward + nextreward;
+                        maxi = max(maxi , totalreward);
+                    }
+                }
+            }
+            dp[row][col1][col2] = maxi;
+        }
+    }
+    }
+    return dp[0][0][n - 1];
     }
     
 };
