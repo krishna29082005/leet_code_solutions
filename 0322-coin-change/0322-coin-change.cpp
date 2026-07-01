e97214966e80e478e7c7ac4396941c11799dcfb1
krishna29082005 <krishnab2908@gmail.com>
2026-07-01 11:37:48 +0530

    Time: 15 ms (92.57%), Space: 17.9 MB (83.63%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int f(int target , vector<int>& coins , vector<int>& dp){
-        if(target == 0) return 0;
-        
-        if(dp[target] != INT_MAX) return dp[target];
-
-        int mini = 1e9;
-        for(int i = 0 ; i < coins.size() ; i++){
-            if(target >= coins[i]){
-                int ans = f(target - coins[i] , coins , dp);
-                if(ans == -1) continue;
-                mini = min(mini , ans + 1);
-            }
-        }
-        if(mini == 1e9) return dp[target] = -1;
-        return dp[target] = mini;
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int>dp(amount + 1 , INT_MAX);
-        int ans = f(amount , coins , dp);
-        return ans;
+        vector<int>dp(amount + 1 , 1e9);
+        dp[0] = 0;
+        for(int i = 1 ; i < amount + 1 ; i++){
+            
+            int mini = 1e9;
+            for(int j = 0 ; j < coins.size() ; j++){  
+
+                if(i >= coins[j]){
+                   int ans = dp[i - coins[j]];
+                   mini = min(mini , ans + 1);
+                }
+            }
+            dp[i] = mini; 
+        }
+        if(dp[amount] == 1e9) return -1;
+        return dp[amount];
     }
 };
