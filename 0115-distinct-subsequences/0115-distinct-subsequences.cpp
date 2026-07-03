bb1698996381d8c4c9320ad43b62ce95c9c737c2
krishna29082005 <krishnab2908@gmail.com>
2026-07-03 18:48:14 +0530

    Time: 8 ms (90.76%), Space: 9.1 MB (85.7%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -11,17 +11,15 @@ public:
             else dp[i] = 0;
         }
         for(int i = 1 ; i < m + 1  ; i++){
-            vector<unsigned long long>temp(n + 1 , 0);
-            temp[0] = 1;
-            for(int j = 1 ; j < n + 1 ; j++){
+        
+            for(int j = n ; j >= 1 ; j--){
                 if(s[i - 1] == t[j - 1]){
-                    temp[j] = dp[j - 1] + dp[j];
+                    dp[j] = dp[j - 1] + dp[j];
                 }
                 else{
-                    temp[j] = dp[j];
+                    dp[j] = dp[j];
                 }
             }
-            dp = temp;
         }
         return (int)dp[n];
     }
