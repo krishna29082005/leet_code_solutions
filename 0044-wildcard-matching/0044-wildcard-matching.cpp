04594409c1e64dee3577895d8eb01d00b08c5993
krishna29082005 <krishnab2908@gmail.com>
2026-07-04 17:30:18 +0530

    Time: 41 ms (70.86%), Space: 12.9 MB (78.89%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,33 +3,33 @@ public:
     bool isMatch(string s, string p) {
         int m = s.size();
         int n = p.size();
-        vector<vector<bool>>dp(m + 1 , vector<bool>(n + 1, false));
-        dp[0][0] = true;
-        for(int i = 1 ; i < m + 1 ; i++){
-            dp[i][0] = false;
-        }
+        vector<bool>dp(n + 1, false);
+        dp[0] = true;
         
         for(int i = 1 ; i < n + 1 ; i++){
            if(p[i - 1] == '*')
-              dp[0][i] = dp[0][i - 1];
+              dp[i] = dp[i - 1];
            else
-              dp[0][i] = false;
+              dp[i] = false;
         }
 
         for(int i = 1 ; i < m + 1 ; i++){
+            vector<bool>temp(n + 1, false);
+            temp[0] = false;
             for(int j = 1 ; j < n + 1 ; j++){
                 if(s[i - 1] == p[j - 1]){
-                   dp[i][j] = dp[i - 1][j - 1];
+                   temp[j] = dp[j - 1];
                 }
                 if(p[j - 1] == '?'){
-                   dp[i][j] = dp[i - 1][ j - 1];
+                   temp[j] = dp[ j - 1];
                 }
 
                 if(p[j - 1] == '*'){
-                   dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                   temp[j] = dp[j] || temp[j - 1];
                 } 
             }
+            dp = temp;
         }
-        return dp[m][n];
+        return dp[n];
     }
 };
