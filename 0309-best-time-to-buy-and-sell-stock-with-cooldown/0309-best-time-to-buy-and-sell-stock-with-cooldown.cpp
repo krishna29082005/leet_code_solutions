520e0593cde8cfe6b39f051d7ee0f7823ce8be26
krishna29082005 <krishnab2908@gmail.com>
2026-07-05 16:22:36 +0530

    Time: 3 ms (32.74%), Space: 15.3 MB (76.06%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n + 2 , vector<int>(2 , -1));
-        for(int i = 0 ; i < 2 ; i++){
-            dp[n + 1][i] = 0;
-            dp[n][i] = 0;
-        }
+        vector<int>dp(2 , 0);
+        vector<int>dpc(2 , 0);
+        
         for(int i = n - 1 ; i >= 0  ; i--){
+            vector<int>temp(2 , 0);
             for(int j = 0 ; j < 2 ; j++){
                 if(j == 1){
-                    int buy = -prices[i] + dp[i + 1][0];
-                    int nbuy = dp[i + 1][1];
-                    dp[i][j] = max(buy , nbuy);
+                    int buy = -prices[i] + dp[0];
+                    int nbuy = dp[1];
+                    temp[j] = max(buy , nbuy);
                 }
                 else{
-                    int sell = prices[i] +  dp[i + 2][1];
-                    int nsell = 0 + dp[i + 1][0];
-                    dp[i][j] = max(sell , nsell);
+                    int sell = prices[i] +  dpc[1];
+                    int nsell = 0 + dp[0];
+                    temp[j] = max(sell , nsell);
                 }
             }
+            dpc = dp;
+            dp = temp;
         }
-        return dp[0][1];
+        return dp[1];
     }
 };
