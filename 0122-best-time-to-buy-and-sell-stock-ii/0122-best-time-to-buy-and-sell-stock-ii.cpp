fc323514b8eb9b2662c2b093f32e2bbfe1ebbeb5
krishna29082005 <krishnab2908@gmail.com>
2026-07-05 13:29:23 +0530

    Time: 7 ms (38.47%), Space: 22.6 MB (37.41%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n + 1 , vector<int>(2 , -1));
-        for(int i = 0 ; i < 2 ; i++){
-            dp[n][i] = 0;
-        }
+        vector<int>dp(2 , 0);
+        
         for(int i = n - 1 ; i >= 0  ; i--){
+            vector<int>temp(2 , 0);
             for(int j = 0 ; j < 2 ; j++){
                 if(j == 1){
-                    int buy = -prices[i] + dp[i + 1][0];
-                    int nbuy = dp[i + 1][1];
-                    dp[i][j] = max(buy , nbuy);
+                    int buy = -prices[i] + dp[0];
+                    int nbuy = dp[1];
+                    temp[j] = max(buy , nbuy);
                 }
                 else{
-                    int sell = prices[i] +  dp[i + 1][1];
-                    int nsell = 0 + dp[i + 1][0];
-                    dp[i][j] = max(sell , nsell);
+                    int sell = prices[i] +  dp[1];
+                    int nsell = 0 + dp[0];
+                    temp[j] = max(sell , nsell);
                 }
             }
+            dp = temp;
         }
-        return dp[0][1];
+        return dp[1];
     }
 };
