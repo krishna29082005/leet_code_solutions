e444fb37b6d74bfae05d3b167d84daf6ca6f61f4
krishna29082005 <krishnab2908@gmail.com>
2026-07-05 14:13:06 +0530

    Time: 534 ms (5.47%), Space: 222.5 MB (32.31%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,35 +2,32 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices){
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n + 1, vector<vector<int>>(2 , vector<int>(3 , -1)));
-        for(int i = 0 ; i < 2 ; i++){
-            for(int j = 0 ; j < 3 ; j++)
-            dp[n][i][j] = 0;
-        }
-        for(int i = 0 ; i < n + 1 ; i++){
-            for(int j = 0 ; j < 2 ; j++){
-                dp[i][j][2] = 0;
+        vector<vector<int>>dp(2 , vector<int>(3 , 0));
+        
+        for(int j = 0 ; j < 2 ; j++){
+                dp[j][2] = 0;
             }
-        }
-
+    
         for(int i = n - 1 ; i >= 0  ; i--){
+            vector<vector<int>>temp(2 , vector<int>(3 , 0));
             for(int j = 0 ; j < 2 ; j++){
                 for(int k = 1 ; k >= 0 ; k--){
                     if(j == 1){
-                        int buy = -prices[i] + dp[i + 1][0][k];
+                        int buy = -prices[i] + dp[0][k];
 
-                        int nbuy = 0 + dp[i + 1][1][k];
-                        dp[i][j][k] = max(buy , nbuy);
+                        int nbuy = 0 + dp[1][k];
+                        temp[j][k] = max(buy , nbuy);
                     }
                     else{
-                        int sell = prices[i] +  dp[i + 1][1][k+1];
+                        int sell = prices[i] +  dp[1][k+1];
                         
-                        int nsell = 0 + dp[i + 1][0][k];
-                        dp[i][j][k] = max(sell , nsell);
+                        int nsell = 0 + dp[0][k];
+                        temp[j][k] = max(sell , nsell);
                     }
                 }
             }
+            dp = temp;
         }
-        return dp[0][1][0];
+        return dp[1][0];
     }
 };
