9f08d582221929e4b0843cb0c925f8c21699d396
krishna29082005 <krishnab2908@gmail.com>
2026-07-09 11:56:35 +0530

    Time: 98 ms (74.11%), Space: 13.7 MB (91.17%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int f(int i , int j , vector<int>& nums , vector<vector<int>>&dp){
-        if(i > j) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int maxi = 0;
-        for(int k = i ; k <= j ; k++){
-            int left = f(i , k - 1 , nums , dp);
-            int right = f(k + 1 , j , nums , dp);
-            int merge = nums[i - 1]*nums[k]*nums[j + 1];
-            int sum = left + right + merge;
-            maxi = max(sum , maxi);
-        }
-        return dp[i][j] = maxi;
-    }
     int maxCoins(vector<int>& nums) {
         nums.push_back(1);
         nums.insert(nums.begin() , 1);
         int n = nums.size();
         vector<vector<int>>dp(n , vector<int>(n , -1));
-        int ans = f(1 , n - 2 , nums , dp);
-        return ans;
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i > j) dp[i][j] = 0;
+            }
+        }
+
+        for(int i = n - 1 ; i >= 1 ; i--){
+            for(int j = i ; j < n - 1 ; j++){
+                int maxi = 0;
+                for(int k = i ; k <= j ; k++){
+                   int left = dp[i][k - 1];
+                   int right = dp[k + 1][j];
+                   int merge = nums[i - 1]*nums[k]*nums[j + 1];
+                   int sum = left + right + merge;
+                   maxi = max(sum , maxi);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n - 2];
     }
 };
