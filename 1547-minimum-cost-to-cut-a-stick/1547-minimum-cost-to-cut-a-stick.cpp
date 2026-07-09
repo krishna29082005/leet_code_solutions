2e7ad17a4c11c8b83a143f362efb04219f58121c
krishna29082005 <krishnab2908@gmail.com>
2026-07-09 10:54:53 +0530

    Time: 45 ms (5.52%), Space: 13.9 MB (53.32%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
+    int f(int i , int j , vector<int>& cuts , vector<vector<int>>&dp){
+        if(i > j) return 0;
+        
+        int mini = INT_MAX;
+        if(dp[i][j] != -1) return dp[i][j];
+        for(int k = i ; k <= j ; k++){
+
+            int left = f(i , k - 1 , cuts , dp);
+            int right = f(k + 1 , j , cuts , dp);
+            int merge = cuts[j + 1] - cuts[i - 1];
+            int sum = left + right + merge;
+            mini = min(mini , sum);
+        }
+        return dp[i][j] = mini;
+    }
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(n);
         cuts.insert(cuts.begin() , 0);
-
-        sort(cuts.begin() , cuts.end());
+  
         int m = cuts.size();
-        vector<vector<int>>dp(cuts.size() , vector<int>(cuts.size() , -1));
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(i > j)dp[i][j] =0;
-            }
-        }
-
-        
-        
-        for(int i = m - 2 ; i >= 1 ; i--){
-            for(int j = i ; j < m - 1 ; j++){
-                int mini = INT_MAX;
-                for(int k = i ; k <= j ; k++){
-                    
-                    int left = dp[i][k - 1];
-                    int right = dp[k + 1][j];
-                    
-                    int cost = cuts[j + 1] - cuts[i - 1];
-                    int sum = left + right + cost;
-                    mini = min(sum , mini);
-                }
-                dp[i][j] = mini;
-            }
-        }
-        return dp[1][m - 2];
-    }
+        vector<vector<int>>dp(m , vector<int>(m , -1));
+        sort(cuts.begin() , cuts.end());
+        int ans = f(1 , m - 2 , cuts , dp);
+        return ans;
+    } 
 };
