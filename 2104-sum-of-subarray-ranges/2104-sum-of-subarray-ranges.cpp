d6c252e7648b02cc05addd6443e357216d481f05
krishna29082005 <krishnab2908@gmail.com>
2026-07-18 13:52:29 +0530

    Time: 10 ms (26.48%), Space: 16 MB (15.15%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,109 +1,108 @@
 class Solution {
 public:
-    vector<int> presmall(vector<int>& arr){
-        stack<int>st;
+     vector<int>nextsmall(vector<int>& arr){
         int n = arr.size();
+        stack<int>st;
+        vector<int>ns(n);
+        for(int i = n - 1; i >= 0 ; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+               st.pop();
+            }
+            if(st.empty()){
+                ns[i] = n;
+                st.push(i);
+            }
+            else{
+                ns[i] = st.top();
+                st.push(i);
+            }
+        }
+        return ns;
+    }
+
+    vector<int>nextbig(vector<int>& arr){
+        int n = arr.size();
+        stack<int>st;
+        vector<int>nb(n);
+        for(int i = n - 1; i >= 0 ; i--){
+            while(!st.empty() && arr[i] >= arr[st.top()]){
+               st.pop();
+            }
+            if(st.empty()){
+                nb[i] = n;
+                st.push(i);
+            }
+            else{
+                nb[i] = st.top();
+                st.push(i);
+            }
+        }
+        return nb;
+    }
+
+    
+    vector<int>prevsmall(vector<int>& arr){
+        int n = arr.size();
+        stack<int>st;
         vector<int>ps(n);
         for(int i = 0 ; i < n ; i++){
-        while(!st.empty() && arr[i] < arr[st.top()]){
-            st.pop();
+            while(!st.empty() && arr[st.top()] > arr[i]){
+               st.pop();
+            }
+            if(st.empty()){
+                ps[i] = -1;
+                st.push(i);
+            }
+            else{
+                ps[i] = st.top();
+                st.push(i);
+            }
         }
-
-        if(st.empty()){
-            ps[i] = -1;
-            st.push(i);
-        }
-        else{
-            ps[i] = st.top();
-            st.push(i);
-        }
+        return ps;
     }
-    return ps;
-    }
 
-    vector<int> nextsmall(vector<int>& arr){
-        stack<int>st;
+    vector<int>prevbig(vector<int>& arr){
         int n = arr.size();
-        vector<int>ns(n);
-        for(int i = n - 1 ; i >= 0 ; i--){
-        while(!st.empty() && arr[i] <= arr[st.top()]){
-            st.pop();
-        }
-
-        if(st.empty()){
-            ns[i] = arr.size();
-            st.push(i);
-        }
-        else{
-            ns[i] = st.top();
-            st.push(i);
-        }
-    }
-    return ns;
-    }
-    vector<int> prebig(vector<int>& arr){
         stack<int>st;
-        int n = arr.size();
         vector<int>pb(n);
         for(int i = 0 ; i < n ; i++){
-        while(!st.empty() && arr[i] > arr[st.top()]){
-            st.pop();
+            while(!st.empty() && arr[st.top()] < arr[i]){
+               st.pop();
+            }
+            if(st.empty()){
+                pb[i] = -1;
+                st.push(i);
+            }
+            else{
+                pb[i] = st.top();
+                st.push(i);
+            }
         }
+        return pb;
+    }
 
-        if(st.empty()){
-            pb[i] = -1;
-            st.push(i);
-        }
-        else{
-            pb[i] = st.top();
-            st.push(i);
-        }
-    }
-    return pb;
-    }
-
-    vector<int> nextbig(vector<int>& arr){
-        stack<int>st;
-        int n = arr.size();
-        vector<int>nb(n);
-        for(int i = n - 1 ; i >= 0 ; i--){
-        while(!st.empty() && arr[i] >= arr[st.top()]){
-            st.pop();
-        }
-
-        if(st.empty()){
-            nb[i] = arr.size();
-            st.push(i);
-        }
-        else{
-            nb[i] = st.top();
-            st.push(i);
-        }
-    }
-    return nb;
-    }
     long long subArrayRanges(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>ns;
-        vector<int>ps;
-        vector<int>nb;
-        vector<int>pb;
-        ns = nextsmall(nums);
-        ps = presmall(nums);
-        nb = nextbig(nums);
-        pb = prebig(nums);
+     int n = nums.size();
+     vector<int>ps;  
+     vector<int>pb; 
+     vector<int>ns; 
+     vector<int>nb; 
 
-        long long temp1 = 0;
-        long long temp2 = 0;
-        for(int i = 0 ; i < n ; i++){
-            long long lc = i - ps[i];
-            long long rc = ns[i] - i;
-            temp1 += nums[i]*lc*rc;
-            
-            long long lb = i - pb[i];
-            long long rb = nb[i] - i;
-            temp2 += nums[i]*lb*rb;   
-        }
-        return temp2 - temp1;
+     ps = prevsmall(nums);
+     pb = prevbig(nums);
+     ns = nextsmall(nums);
+     nb = nextbig(nums);
+     long long temp1 = 0;
+     long long temp2 = 0;
+     for(int i = 0 ; i < n ; i++){
+        long long lc = i - ps[i];
+        long long rc = ns[i] - i;
+        temp1 += 1LL*lc*rc*nums[i];
+
+        long long lc1 = i - pb[i];
+        long long rc1 = nb[i] - i;
+        temp2 += 1LL*lc1*rc1*nums[i];
+     } 
+     return temp2 - temp1;
     }
 };
