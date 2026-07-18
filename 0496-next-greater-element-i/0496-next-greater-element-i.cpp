3d58a4f6c7b1a3e34a61bc441859768b4c4e595d
krishna29082005 <krishnab2908@gmail.com>
2026-07-18 11:01:45 +0530

    Time: 0 ms (100%), Space: 12.9 MB (15.74%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,31 +3,25 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
+
         map<int , int>mp;
         stack<int>st;
-
         for(int i = n2 - 1 ; i >= 0 ; i--){
-
-            while(!st.empty() && nums2[i] >= st.top()){
-               st.pop();
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
             }
-
-            if(st.empty()) 
-            {
+            if(st.empty()){
                 mp[nums2[i]] = -1;
                 st.push(nums2[i]);
-            }else{
+            }
+            else{
                 mp[nums2[i]] = st.top();
                 st.push(nums2[i]);
             }
-        } 
+        }
         vector<int>ans;
         for(int i = 0 ; i < n1 ; i++){
-            if(mp.find(nums1[i]) != mp.end()){
-                ans.push_back(mp[nums1[i]]);
-            }else{
-                ans.push_back(-1);
-            }
+           ans.push_back(mp[nums1[i]]);
         }
         return ans;
     }
