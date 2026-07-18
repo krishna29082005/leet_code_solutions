cec764a6fc997334d698dc601d0f2cf81ff21e34
krishna29082005 <krishnab2908@gmail.com>
2026-07-18 12:13:53 +0530

    Time: 0 ms (100%), Space: 27.1 MB (37.52%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -6,20 +6,17 @@ public:
        stack<int>st;
        int water = 0;
        for(int i = 0 ; i < n ; i++){
-        
         while(!st.empty() && height[i] > height[st.top()]){
             int bottom = st.top();
             st.pop();
 
-            if(st.empty()){
-                break;
-            }
+            if(st.empty()) break;
 
             int leftw = st.top();
 
             int wid = i - leftw - 1;
 
-            int lev = min(height[leftw] , height[i]) - height[bottom];
+            int lev = min(height[i] , height[leftw]) - height[bottom];
 
             water += lev*wid;
         }
