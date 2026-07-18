0f11df46734e126df604fe6093c9acee2842bc7a
krishna29082005 <krishnab2908@gmail.com>
2026-07-18 14:13:39 +0530

    Time: 7 ms (66.5%), Space: 23.1 MB (15.32%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,56 +1,68 @@
 class Solution {
 public:
+    vector<int>nextsmall(vector<int>& arr){
+        int n = arr.size();
+        stack<int>st;
+        vector<int>ns(n);
+        for(int i = n - 1; i >= 0 ; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+               st.pop();
+            }
+            if(st.empty()){
+                ns[i] = n;
+                st.push(i);
+            }
+            else{
+                ns[i] = st.top();
+                st.push(i);
+            }
+        }
+        return ns;
+    }
+
+    vector<int>prevsmall(vector<int>& arr){
+        int n = arr.size();
+        stack<int>st;
+        vector<int>ps(n);
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
+               st.pop();
+            }
+            if(st.empty()){
+                ps[i] = -1;
+                st.push(i);
+            }
+            else{
+                ps[i] = st.top();
+                st.push(i);
+            }
+        }
+        return ps;
+    }
+
+
     int maximalRectangle(vector<vector<char>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
         int maxi = INT_MIN;
         vector<int>heights(n , 0);
         for(int i = 0 ; i < m ; i++){
+           
             for(int j = 0 ; j < n ; j++){
                 if(matrix[i][j] == '0') heights[j] = 0;
                 else heights[j] += 1;
             }
         
         
-        vector<int>pse(n); 
-        stack<int>st;
-        for(int i = 0 ; i < n ; i++){
-           
-            while(!st.empty() && heights[st.top()] > heights[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                pse[i] = -1;
-                st.push(i);
-            }else{
-                pse[i] = st.top();
-                st.push(i);
-            }
+        vector<int>ps = prevsmall(heights);
+        vector<int>ns = nextsmall(heights);
+        for(int j = 0 ; j < n ; j++){
+        int ls = ps[j];
+        int rs = ns[j];
+        int wid = rs - ls - 1;
+        int area = heights[j]*wid;
+        maxi = max(maxi , area);
         }
-
-        stack<int>st1;
-        vector<int>nse(n);
-        
-        for(int i = n - 1; i >= 0 ; i--){
-            
-            while(!st1.empty() && heights[st1.top()] >= heights[i]){
-                st1.pop();
-            }
-            if(st1.empty()){
-                nse[i] = n;
-                st1.push(i);
-            }else{
-                nse[i] = st1.top();
-                st1.push(i);
-            }
-        }
-        
-        for(int i = 0 ; i < n ; i++){
-            int wid = nse[i] - pse[i] - 1;
-            int area = heights[i]*wid;
-            maxi = max(maxi , area);
-        }
-        
     }
     return maxi;
     }
