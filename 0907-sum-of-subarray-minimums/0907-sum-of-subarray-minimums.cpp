7e34abcd3440dd5bdb2330eb01b7ba51de5f908c
krishna29082005 <krishnab2908@gmail.com>
2026-07-18 12:43:00 +0530

    Time: 68 ms (51.84%), Space: 193.7 MB (11.68%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,62 +1,56 @@
 class Solution {
 public:
-    vector<int> presmall(vector<int>& arr){
-        stack<int>st;
+    vector<int>nextsmall(vector<int>& arr){
         int n = arr.size();
+        stack<int>st;
+        vector<int>ns(n);
+        for(int i = n - 1; i >= 0 ; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+               st.pop();
+            }
+            if(st.empty()){
+                ns[i] = n;
+                st.push(i);
+            }
+            else{
+                ns[i] = st.top();
+                st.push(i);
+            }
+        }
+        return ns;
+    }
+
+    vector<int>prevsmall(vector<int>& arr){
+        int n = arr.size();
+        stack<int>st;
         vector<int>ps(n);
         for(int i = 0 ; i < n ; i++){
-        while(!st.empty() && arr[i] < arr[st.top()]){
-            st.pop();
+            while(!st.empty() && arr[st.top()] > arr[i]){
+               st.pop();
+            }
+            if(st.empty()){
+                ps[i] = -1;
+                st.push(i);
+            }
+            else{
+                ps[i] = st.top();
+                st.push(i);
+            }
         }
-
-        if(st.empty()){
-            ps[i] = -1;
-            st.push(i);
-        }
-        else{
-            ps[i] = st.top();
-            st.push(i);
-        }
-    }
-    return ps;
-    }
-
-    vector<int> nextsmall(vector<int>& arr){
-        stack<int>st;
-        int n = arr.size();
-        vector<int>ns(n);
-        for(int i = n - 1 ; i >= 0 ; i--){
-        while(!st.empty() && arr[i] <= arr[st.top()]){
-            st.pop();
-        }
-
-        if(st.empty()){
-            ns[i] = arr.size();
-            st.push(i);
-        }
-        else{
-            ns[i] = st.top();
-            st.push(i);
-        }
-    }
-    return ns;
+        return ps;
     }
     int sumSubarrayMins(vector<int>& arr) {
-        int mod = 1e9 + 7;
-        int n = arr.size();
-        vector<int>ns;
-        vector<int>ps;
-        ns = nextsmall(arr);
-        ps = presmall(arr);
-
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            long long  lc = i - ps[i];
-            long long rc = ns[i] - i;
-            long long temp = (arr[i]*lc*rc)%mod;
-            ans = (ans + temp)%mod;
-            
-        }
-        return ans%mod;
+    int mod = 1e9 + 7;
+    vector<int> ns = nextsmall(arr);
+    vector<int> ps = prevsmall(arr);
+    int ans = 0;
+    int n = arr.size();
+    for(int i = 0 ; i < n ; i++){
+        long long lc = i - ps[i];
+        long long rc = ns[i] - i;
+        long long total = (lc*rc*arr[i])%mod;
+        ans  = (ans + total)%mod;
+    }
+    return ans;
     }
 };
