7cadfcbbe0f1ae311267d3dee59659abf3c63794
krishna29082005 <krishnab2908@gmail.com>
2026-07-20 12:38:36 +0530

    Time: 0 ms (100%), Space: 11.4 MB (65.6%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -6,29 +6,27 @@ public:
         string ans;
         if(num.size() == 1 && k == 1) return "0";
         for(int i = 0 ; i < n ; i++){
-        while(!st.empty() && st.top() > num[i] && k > 0){
-
+            while(!st.empty() && st.top() > num[i]){
+                if(k == 0) break;
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+        }
+        while(k != 0)
+        {
             st.pop();
             k--;
         }
-        st.push(num[i]);
-        }
-        while(k > 0){
-        st.pop();
-        k--;
-        }
         while(!st.empty()){
-            ans.push_back(st.top());
+            ans += st.top();
             st.pop();
         }
-        
         reverse(ans.begin() , ans.end());
-        while(!ans.empty() && ans[0] == '0')
-        ans.erase(0,1);
-        
-        if(ans.empty()){
-            return "0";
+        while(ans.size() > 0 && ans[0] == '0'){
+            ans.erase(0 , 1);
         }
-        return ans; 
+        if(ans.empty()) return "0";
+        return ans;
    }
 };
