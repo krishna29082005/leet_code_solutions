345522fa716b4d7fe87020a9211e9e24960d12d0
krishna29082005 <krishnab2908@gmail.com>
2026-08-12 17:18:33 +0530

    Time: 0 ms (100%), Space: 19.6 MB (95.9%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,35 +12,35 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL) return 0;
-        int ans = INT_MIN;
-        queue<pair<TreeNode* , int>> q;
-        q.push({root , 0});
-        while(!q.empty())
-        {
-            int mmin = q.front().second;
-            int size = q.size();
-            int first , last;
-            for(int i = 0 ; i < size ; i++)
-            {
-                long long curr_id = q.front().second - mmin;
-                TreeNode * temp = q.front().first;
-                q.pop();
-                if(i == 0) first = curr_id;
-                if(i == size - 1) last = curr_id;
-                
-                if(temp -> left != NULL) 
-                {
-                    q.push({temp -> left , 2*curr_id + 1});
-                }
 
-                if(temp -> right != NULL) 
-                {
-                    q.push({temp -> right , 2*curr_id + 2});
+        queue<pair<TreeNode* , int>>q;
+        int ans = -1;
+        if(root == NULL) return 0;
+        q.push({root , 0});
+
+        while(!q.empty()){
+
+            int n = q.size();
+            int mini = q.front().second;
+            int first , last;
+
+            for(int i = 0 ; i < n ; i++){
+                auto it = q.front();
+                q.pop();
+                long long cid = it.second - mini;
+                
+                if(i == 0) first = cid;
+                if(i == n - 1) last = cid;
+
+                if(it.first -> left != NULL){
+                    q.push({it.first -> left , 2*cid + 1});
                 }
-                ans = max(ans, last - first + 1);
-            } 
-        } 
+                if(it.first -> right != NULL){
+                    q.push({it.first -> right , 2*cid + 2});
+                }
+            }
+            ans = max(ans , last - first + 1);
+        }
         return ans;
     } 
 };
