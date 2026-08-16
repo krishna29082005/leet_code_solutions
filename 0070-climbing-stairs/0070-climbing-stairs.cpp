e408433f13d228f756dda5794e495f76bc99faa5
krishna29082005 <krishnab2908@gmail.com>
2026-08-16 20:59:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,15 @@
 class Solution {
-private: 
-    int ways(int n , vector<int>&dp){
-        if(n <= 1) return 1;
+private:
+    int helper(int n , vector<int>& dp){
+        if(n <= 1){
+            return dp[n] = 1;
+        }
         if(dp[n] != -1) return dp[n];
-        return dp[n] = ways(n - 1 , dp) + ways(n - 2 , dp);
+        return dp[n] = helper(n - 1 , dp) + helper(n - 2 , dp);
     }
 public:
     int climbStairs(int n) {
-       vector<int>dp(n + 1 , -1);
-       int ans = ways(n , dp);
-       return ans; 
+       vector<int> dp(n + 1 , -1);
+       return helper(n , dp);
     }
 };
