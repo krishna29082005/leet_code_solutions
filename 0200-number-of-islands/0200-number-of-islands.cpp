76e8bf581ae8bda807e8d628a7a93702339f11e1
krishna29082005 <krishnab2908@gmail.com>
2026-08-18 13:34:25 +0530

    Time: 31 ms (27.93%), Space: 24.5 MB (13.1%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,43 +1,47 @@
 class Solution {
 public:
-    
-    void bfs(vector<vector<char>>& grid , vector<vector<int>> &vis
-    ,queue<pair<int , int>>& q , int m , int n){
-        int delrow[4] = {-1 , 0 , 1 , 0};
-        int delcol[4] = {0 , 1 , 0 , -1};
+    void bfs(int i , int j , vector<vector<char>>& grid , vector<vector<int>>& vis){
+        queue<pair<int , int>>q;
+        q.push({i , j});
+
+        int n = grid.size();
+        int m = grid[0].size();
+
         while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            vis[r][c] = 1;
-            for(int i = 0 ; i < 4 ; i++){
-                int nrow = r + delrow[i];
-                int ncol = c + delcol[i];
-                if(nrow >=0 && nrow < m && ncol >= 0 && ncol < n && 
-                   grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0){
-                   vis[nrow][ncol] = 1;
-                   q.push({nrow , ncol});
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        int dr[4] = {-1 , 0 , 1 , 0};
+        int dc[4] = {0 , 1 , 0 , -1};
+        for(int i = 0 ; i < 4 ; i++){
+            int nr = x + dr[i];
+            int nc = y + dc[i];
+            if(nr >= 0 && nr < n && nc >=0 && nc < m){
+                if(grid[nr][nc] == '1' && vis[nr][nc] == -1){
+                    q.push({nr , nc});
+                    vis[nr][nc] = 1;
+                }
             }
-        }
         }
     }
+}
     int numIslands(vector<vector<char>>& grid) {
-    int m = grid.size();
-    int n = grid[0].size();
-    int cnt = 0;
-    vector<vector<int>> vis(m, vector<int>(n, 0));
-    
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(grid[i][j] == '1' && vis[i][j] == 0){
-                queue<pair<int , int>>q;
-                cnt++;
-                vis[i][j] = 1;
-                q.push({i , j});
-                bfs(grid , vis , q , m , n);
-            }
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n , vector<int>(m , -1));
+        int cnt = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+
+                if(grid[i][j] == '1' && vis[i][j] == -1){
+                    vis[i][j] = 1;
+                    cnt++;
+                    bfs(i , j , grid , vis);
+                }
             }
         }
-        return cnt;  
+        return cnt;
     }
 };
