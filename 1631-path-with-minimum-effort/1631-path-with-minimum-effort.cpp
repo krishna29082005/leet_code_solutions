970477714ef851282c2d9499e5e0000a15d20c38
krishna29082005 <krishnab2908@gmail.com>
2026-08-24 19:25:08 +0530

    Time: 63 ms (39.58%), Space: 24.2 MB (97.36%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n = heights.size();
-        int m = heights[0].size();
+       int n = heights.size();
+       int m = heights[0].size();
+       
+       priority_queue<pair<int , pair<int , int>> , vector<pair<int , pair<int , int>>> , greater<pair<int , pair<int , int>>>>q;
+       vector<vector<int>>dis(n , vector<int>(m , INT_MAX));
+       q.push({0 , {0 , 0}});
+       dis[0][0] = 0;
 
-        priority_queue<pair<int , pair<int , int>>,
-        vector<pair<int , pair<int , int>>>,
-        greater<pair<int , pair<int , int>>>>pq;
-        vector<vector<int>> vis(n , vector<int>(m , INT_MAX));
-        pq.push({0 , { 0 , 0}});
-        vis[0][0] = 0;
-        int delrow[4] = {-1 , 0 , 1 , 0};
-        int delcol[4] = { 0 , 1 , 0 ,-1};
-        while(!pq.empty()){
-            auto it = pq.top();
-            int dis = it.first;
-            int i = it.second.first;
-            int j = it.second.second;
-            if(i == n-1 && j == m-1) return dis;
-            pq.pop();
-            if(dis > vis[i][j]) continue;
-            for(int x = 0 ; x < 4 ; x++){
-                int r = i + delrow[x];
-                int c = j + delcol[x];
-                if(r >= 0 && r < n && c >= 0 && c < m)
-                { 
-                int tdis = abs(heights[i][j] - heights[r][c]);  
-                int newEffort = max(dis, tdis);
+       while(!q.empty()){
+        int maxeff = q.top().first;
+        int x = q.top().second.first;
+        int y = q.top().second.second;
+        q.pop();
 
-                if(newEffort < vis[r][c]){
-                   vis[r][c] = newEffort;
-                   pq.push({newEffort, {r, c}});
-               }
+        int dr[4] = {-1 , 0 , 1 , 0};
+        int dc[4] = {0 , 1 , 0 , -1};
+
+        for(int i = 0 ; i < 4 ; i++){
+            int nx = x + dr[i];
+            int ny = y + dc[i];
+            
+            if(nx >= 0 && nx < n && ny >=0 && ny < m){
+
+            int neweff = max(maxeff , abs(heights[nx][ny] - heights[x][y]));
+
+            if(dis[nx][ny] > neweff){
+                dis[nx][ny] = neweff;
+                q.push({neweff , {nx , ny}});
             }
-        }
-        }
-        return -1;
+            }
+          } 
+       }
+       return dis[n - 1][m - 1];
     }
 };
