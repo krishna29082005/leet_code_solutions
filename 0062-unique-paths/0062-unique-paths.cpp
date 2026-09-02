86b05c57bdca76ac1c8d3dbe1ccade105bd5552b
krishna29082005 <krishnab2908@gmail.com>
2026-09-02 13:31:42 +0530

    Time: 0 ms (100%), Space: 8.1 MB (87.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,41 +1,31 @@
 class Solution {
 private: 
-    int helper(int m  , int n , vector<vector<int>>& dp){
-        if(m == 0 && n == 0) return dp[m][n] = 1;
-         
-        if(dp[m][n] != -1) return dp[m][n];
-
-        int up = 0;
-        if(m - 1 >= 0){
-           up = helper(m - 1 , n , dp);
-        }
-
-        int left = 0;
-        if(n - 1 >= 0){
-           left = helper(m , n - 1 , dp);
-        }
-        return dp[m][n] = left + up;
-    }
+   
 public:
     int uniquePaths(int m, int n) {
-     vector<vector<int>>dp(m , vector<int>(n , -1));
-     dp[0][0] = 1;
+     vector<int>dp(n);
+     
      for(int i = 0 ; i < m ; i++)
      {
+        vector<int>temp(n);
         for(int j = 0 ; j < n ; j++){
-            if(i == 0 &&j == 0) continue;
+            if(i == 0 && j == 0){
+                temp[j] = 1;
+                continue;
+            }
 
             int up = 0;
             int left = 0;
             if(i - 1 >= 0){
-                up = dp[i - 1][j];
+                up = dp[j];
             }
             if(j - 1 >= 0){
-                left = dp[i][j - 1];
+                left = temp[j - 1];
             }
-            dp[i][j] = up + left;
+            temp[j] = up + left;
         }
+        dp = temp;
      }
-     return dp[m - 1][n - 1];   
+     return dp[n - 1];   
     }
 };
