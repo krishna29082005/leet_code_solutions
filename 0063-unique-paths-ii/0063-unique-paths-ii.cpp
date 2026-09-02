a670afb8d50ae286ba5a5d5cb37ae6f22d4704f1
krishna29082005 <krishnab2908@gmail.com>
2026-09-02 13:37:08 +0530

    Time: 0 ms (100%), Space: 11.9 MB (31.63%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,42 +1,32 @@
 class Solution {
+private:
+   int helper(int m  , int n , vector<vector<int>>&grid , vector<vector<int>>& dp){
+        if(m == 0 && n == 0) return dp[m][n] = 1;
+        if(grid[m][n] == 1) return dp[m][n] = 0;
+
+        if(dp[m][n] != -1) return dp[m][n];
+
+        int up = 0;
+        if(m - 1 >= 0){
+           up = helper(m - 1 , n , grid , dp);
+        }
+
+        int left = 0;
+        if(n - 1 >= 0){
+           left = helper(m , n - 1 , grid , dp);
+        }
+        return dp[m][n] = left + up;
+    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<int>dp(n , -1);
+        vector<vector<int>>dp(m , vector<int>(n , -1));
         if (obstacleGrid[0][0] == 1)
            return 0;
-        dp[0] = 1;
-        for(int i = 0 ; i < m ; i++){
-            vector<int> temp(n);
-            for(int j = 0 ; j < n ; j++){
-
-                if(obstacleGrid[i][j] == 1) 
-                {
-                    temp[j] = 0;
-                    continue;
-                }
-                if(i == 0 && j == 0) 
-                {
-                    temp[j] = 1;
-                    continue;
-                }
-                
-                int up = 0;
-                int left = 0;
-                if(i - 1 >= 0 && i - 1 < m ) // going up
-                {
-                    up = dp[j];
-                }
-               if(j - 1 >= 0 && j - 1 < n ) // going left
-                {
-                    left = temp[j - 1];
-                } 
-                temp[j] = up + left;
-            }
-            dp = temp;
-        }
-        return dp[n - 1];
+       
+        
+        return helper(m - 1 , n - 1 , obstacleGrid , dp);
     }
 };
