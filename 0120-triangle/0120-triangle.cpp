3f287144c23959d892ba29078d5cd8a2bd70284d
krishna29082005 <krishnab2908@gmail.com>
2026-09-03 09:36:03 +0530

    Time: 0 ms (100%), Space: 13.4 MB (25.67%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -18,32 +18,33 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
        
        int n = triangle.size();
-       vector<int> dp(n);
-       dp[0] = triangle[0][0];
+       vector<vector<int>> dp(n , vector<int>(n , INT_MAX));
+       return helper(0 , 0 , triangle , n , dp);
+    //    dp[0] = triangle[0][0];
 
 
-       for(int i = 1 ; i < n ; i++){
-        int size = triangle[i].size();
-        vector<int>temp(n);
-        for(int j = 0 ; j < size ; j++){
+    //    for(int i = 1 ; i < n ; i++){
+    //     int size = triangle[i].size();
+    //     vector<int>temp(n);
+    //     for(int j = 0 ; j < size ; j++){
             
-            int a0 = 1e9;
-            int a1 = 1e9;
+    //         int a0 = 1e9;
+    //         int a1 = 1e9;
             
-            if(j >= 0 && j < i){
-                a0 = triangle[i][j] + dp[j];
-            }
-            if(j - 1 >= 0 && j - 1 < i){
-                a1 = triangle[i][j] + dp[j - 1];
-            }
-            temp[j] = min(a0 , a1);
-        }
-        dp = temp;
-       }
-       int mini = INT_MAX;
-       for(int i = 0 ; i < n ; i++){
-        mini = min(mini , dp[i]);
-       }
-       return mini;
+    //         if(j >= 0 && j < i){
+    //             a0 = triangle[i][j] + dp[j];
+    //         }
+    //         if(j - 1 >= 0 && j - 1 < i){
+    //             a1 = triangle[i][j] + dp[j - 1];
+    //         }
+    //         temp[j] = min(a0 , a1);
+    //     }
+    //     dp = temp;
+    //    }
+    //    int mini = INT_MAX;
+    //    for(int i = 0 ; i < n ; i++){
+    //     mini = min(mini , dp[i]);
+    //    }
+    //    return mini;
     }
 };
