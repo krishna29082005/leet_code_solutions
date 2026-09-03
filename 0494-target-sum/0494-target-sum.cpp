3b4b657a99b5fb86e223b461ebb4e0819072715b
krishna29082005 <krishnab2908@gmail.com>
2026-09-03 19:00:25 +0530

    Time: 3 ms (86.03%), Space: 15.2 MB (59.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -3,40 +3,38 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         
         int sum = 0;
+        int n = nums.size();
         for(int i = 0 ; i < nums.size() ; i++){
             sum += nums[i];
         }
 
         if(sum < abs(target)) return 0;
+
         sum = abs(sum - target);
-        if(sum % 2 == 1) return 0;
+
+        if(sum%2 == 1) return 0;
         sum = sum/2;
 
         vector<int>dp(sum + 1 , -1);
-        for(int i = 0 ; i < sum + 1 ; i++){// we know that we only need the last row 
-        //to know the values.
-            if(i == 0)
-            dp[i] = 1;
-            
-            else 
-            dp[i] = 0;
+        for(int i = 0 ; i < sum + 1 ; i++){
+            if(i == 0) dp[i] = 1;
+            else dp[i] = 0;
         }
-       
-        for(int i = nums.size() - 1 ; i >= 0 ; i--) {
-            vector<int>temp(sum + 1 , 0);
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            vector<int>temp(sum + 1 , -1);
+            temp[0] = 1;
             for(int j = 0 ; j < sum + 1 ; j++){
-                
                 int pick = 0;
-                if(nums[i] <= j)//picking the current element
+                if(j >= nums[i]){
                     pick = dp[j - nums[i]];
-                
-                
+                }
                 int nonpick = dp[j];
-                            //not picking the current element
+
                 temp[j] = pick + nonpick;
             }
             dp = temp;
-        }
+        } 
         return dp[sum];
     }
 };
