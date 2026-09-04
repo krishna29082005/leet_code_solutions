f9447d453c3e504952404b28bcc8751e4bcf8730
krishna29082005 <krishnab2908@gmail.com>
2026-09-04 16:54:23 +0530

    Time: 0 ms (100%), Space: 13.1 MB (61.39%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,30 +1,27 @@
 class Solution {
+private:
+int f(int i , int j , int n , int m , string& word1 , string& word2 , vector<vector<int>>&dp){
+   
+    if(i == n && j == m) return 0;
+    if(i == n && j < m) return m - j;
+    if(i < n && j == m) return n - i;
+   
+    if(dp[i][j] != -1) return dp[i][j];
+
+    if(word1[i] == word2[j]){
+        return f(i + 1 , j + 1 , n , m , word1 , word2 , dp);
+    }
+    int del = 1 + f(i + 1 , j , n , m , word1 , word2 , dp);
+    int ins = 1 + f(i , j + 1 , n , m , word1 , word2 , dp);
+    int rep = 1 + f(i + 1 , j + 1 , n , m , word1 , word2 , dp);
+
+    return dp[i][j] = min(del , min(ins , rep));
+}
 public:
     int minDistance(string word1, string word2) {
-        int m = word1.size();
-        int n = word2.size();
-        vector<vector<int>>dp(m + 1 , 
-                              vector<int>(n + 1 , 0));
-        dp[0][0] = 0;
-        for(int j = 1 ; j < n + 1 ; j++){
-            dp[0][j] = j;//because its no more 0 based we made it 1 based
-        }
-        for(int i = 1 ; i < m + 1 ; i++){
-            dp[i][0] = i;//same
-        }
-        
-        for(int i = 1 ; i < m + 1 ; i++){
-           for(int j = 1 ; j < n + 1 ; j++){
-            if(word1[i - 1] == word2[ j - 1]){
-                dp[i][j] = dp[i - 1][j - 1];
-                continue;
-            }
-            int del = 1 + dp[i - 1][j];
-            int rep = 1 + dp[i - 1][j - 1];
-            int ins = 1 + dp[i][j - 1];
-            dp[i][j] = min(del , min(rep , ins));
-           }
-        }
-        return dp[m][n];
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>>dp(n , vector<int>(m , -1));
+        return f(0 , 0 , n , m , word1 , word2 , dp);
     }
 };
