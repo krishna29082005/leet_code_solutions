c08f6f271b014d2ecc50c644e5e439e0df602238
krishna29082005 <krishnab2908@gmail.com>
2026-09-04 10:02:25 +0530

    Time: 55 ms (11.23%), Space: 36.8 MB (34.04%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int f(int index , int target , vector<int>& coins , vector<vector<int>>&dp){
-        if(target == 0) return 1;
-        if(index == coins.size()) return 0;
+    int helper(int i , int target , vector<int>&coins , vector<vector<int>>&dp){
+        if(target < 0) return 0;
 
-        if(dp[index][target] != -1) return dp[index][target];
-
-        int pick = 0;
-        if(target >= coins[index]){
-            pick = f(index , target - coins[index] , coins , dp);
+        if(i == coins.size()){
+            if(target == 0) return 1;
+            return 0;
         }
-        int nonpick = f(index + 1 , target , coins , dp);
+        if(dp[i][target] != -1) return dp[i][target];
         
-        return dp[index][target] = pick + nonpick;
+        int pick = helper(i , target - coins[i] , coins , dp);
+        
+        int nonpick = helper(i + 1, target , coins , dp);
+
+        return dp[i][target] = pick + nonpick;
     }
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
         vector<vector<int>>dp(coins.size() , vector<int>(amount + 1 , -1));
-        int ans = f(0 , amount , coins , dp);
+        int ans = helper(0 , amount , coins , dp);
         return ans;
     }
 };
