20c77d461c31b5fba314a967a4bf9b7cbc0d7580
krishna29082005 <krishnab2908@gmail.com>
2026-09-05 12:28:18 +0530

    Time: 14 ms (32.36%), Space: 18.3 MB (28.32%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,47 +1,30 @@
 class Solution {
+    private:
+    int f(int day , int buy , int cnt , vector<int>& prices , vector<vector<vector<int>>>& dp , int k){
+
+        if(day == prices.size() || cnt == k) return 0;
+        if(dp[day][buy][cnt] != -1) return dp[day][buy][cnt];
+
+        if(buy){
+            int wbuy = -prices[day] + f(day + 1 , 0 , cnt , prices , dp , k);
+
+            int nbuy = 0 + f(day + 1 , 1 , cnt , prices , dp , k);
+
+            return dp[day][buy][cnt] = max(wbuy , nbuy);
+        }else{
+            int wsell = prices[day] + f(day + 1 , 1 , cnt + 1 , prices , dp , k);
+
+            int nsell = 0 + f(day + 1  , 0 , cnt , prices , dp , k);
+            return dp[day][buy][cnt] = max(wsell , nsell);
+        }
+        return dp[day][buy][cnt] = 0;
+    }
 public:
     
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-
-        // dp[day][buy][cnt]
-        vector<vector<vector<int>>> dp(
-            n + 1,
-            vector<vector<int>>(2, vector<int>(k + 1, 0))
-        );
-
-        // Base Cases:
-        // dp[n][buy][cnt] = 0  (already initialized)
-        // dp[day][buy][2] = 0  (already initialized)
-
-        for (int day = n - 1; day >= 0; day--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                for (int cnt = k - 1; cnt >= 0; cnt--) {
-
-                    if (buy) {
-                        int take =
-                            -prices[day] + dp[day + 1][0][cnt];
-
-                        int notTake =
-                            dp[day + 1][1][cnt];
-
-                        dp[day][buy][cnt] =
-                            max(take, notTake);
-                    }
-                    else {
-                        int sell =
-                            prices[day] + dp[day + 1][1][cnt + 1];
-
-                        int notSell =
-                            dp[day + 1][0][cnt];
-
-                        dp[day][buy][cnt] =
-                            max(sell, notSell);
-                    }
-                }
-            }
-        }
-
-        return dp[0][1][0];
+        vector<vector<vector<int>>>dp(n , vector<vector<int>>(2 , vector<int>(k + 1 , -1)));
+        int ans = f(0 , 1 , 0 , prices , dp , k);
+        return ans;
     }
 };
